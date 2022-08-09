f29ad4cee9ae259e1e0adb65e3164fc435d17aa6
adtiya-kumar <dedhaaditya24@gmail.com>
2022-08-09 23:11:48 +0530

    Recursion.Java

---
+++ src/com/company/Recursion.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Recursion {
+    static int factorial(int n) {
+        if(n==0 || n==1)
+            return 1;
+        else
+            return n* factorial(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        System.out.println("factorial of n is :" + factorial(n));
+    }
+}
